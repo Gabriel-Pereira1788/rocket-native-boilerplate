fa8132bef8bd88143b06bb47db343e084100dd79
Gabriel-Pereira1788 <gabriel.pereira1788@gmail.com>
2024-09-29 20:41:45 -0300

    change script js

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
 #!/usr/bin/env node
-const ora = require("ora");
+// const { execSync } = require("child_process");
 
-const spinner = ora("Executing post init script ");
-
-new Promise((resolve) => {
-    spinner.start();
-    // do something
-    resolve();
-})
-    .then(() => {
-        spinner.succeed();
-    })
-    .catch(() => {
-        spinner.fail();
-        throw new Error(
-            "Something went wrong during the post init script execution"
-        );
-    });
+console.log("Welcome to rocket native.🚀 \n");
