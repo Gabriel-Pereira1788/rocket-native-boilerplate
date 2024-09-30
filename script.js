56bdb4f27e266fef5cea8403c53e4b8e3583a822
Gabriel-Pereira1788 <gabriel.pereira1788@gmail.com>
2024-09-29 21:47:17 -0300

    change script js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
 
-console.log("Welcome to rocket native.🚀 \n");
+new Promise((resolve) => {
+    console.log("Welcome to rocket native.🚀 \n");
+    resolve();
+})
+    .then(() => {
+        console.log("ROCKET NATIVE 1.0.🚀 \n");
+    })
+    .catch((error) => {
+        console.error(`🚨 An error occurred with post init script: ${error}`);
+        throw new Error();
+    });
